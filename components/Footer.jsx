5deba36fd559dda82f3a6d4956fe258a334024e5
bellions <ibrabuhari01@gmail.com>
2022-11-25 18:49:28 +0100

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter} from "react-icons/ai"
+
+function Footer() {
+    return (
+      <div className="footer-container">
+        <p>2022 bellions Headphones All Right Resevered.</p>
+        <p className="icons">
+          <AiFillInstagram />
+          <AiOutlineTwitter />
+        </p>
+      </div>
+    )
+  }
+
+export default Footer
